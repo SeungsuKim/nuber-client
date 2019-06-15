d9cca5c9bcf69b9af099bd201a841d0674c91820
seungsukim <seungsu0407@gmail.com>
2019-06-15 16:57:19 +0900

    #2.3 Apollo Setup

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,40 @@
 import ApolloClient, { Operation } from "apollo-boost";
 
 const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      auth: {
+        __typename: "Auth",
+        isLoggedIn: Boolean(localStorage.getItem("token"))
+      }
+    },
+    resolvers: {
+      Mutation: {
+        signUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem("token", token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: "Auth",
+                isLoggedIn: true
+              }
+            }
+          });
+          return null;
+        },
+        signUserOut: (_, __, { cache }) => {
+          localStorage.removeItem("token");
+          cache.writeData({
+            data: {
+              __typename: "Auth",
+              isLoggedIn: false
+            }
+          });
+          return null;
+        }
+      }
+    }
+  },
   request: async (operation: Operation) => {
     operation.setContext({
       headers: {
