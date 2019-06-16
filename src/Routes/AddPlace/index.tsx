c0d5ebc79a3db9843263fbb4cb26ec32ae96bb65
seungsukim <seungsu0407@gmail.com>
2019-06-16 20:14:11 +0900

    #2.10 Planning the Routes

---
+++ src/Routes/AddPlace/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AddPlace = () => <span>Add Place</span>;
+
+export default AddPlace;
---
+++ src/Routes/PhoneSignIn/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PhoneSignIn = () => <span>PhoneSignIn</span>;
+
+export default PhoneSignIn;
